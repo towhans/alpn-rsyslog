249d08a1afbd070473c8dfd54bab842f8bed23ab
Christian Kniep <christian@qnib.org>
2016-04-15 11:04:02 +0200

    initial commit

---
+++ opt/qnib/rsyslog/bin/configure-targets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DEFAULT=true
+if [ "X${FORWARD_TO_ELASTICSEARCH}" == "Xtrue" ];then
+   ln -sf /etc/rsyslog.d/elasticsearch.conf.disabled /etc/rsyslog.d/elasticsearchconf  
+   DEFAULT=false
+fi
+if [ "X${FORWARD_TO_KAFKA}" == "Xtrue" ];then
+   ln -sf /etc/rsyslog.d/kafka.conf.disabled /etc/rsyslog.d/kafka.conf  
+   DEFAULT=false
+fi
+if [ "X${FORWARD_TO_HEKA}" == "Xtrue" ];then
+   ln -sf /etc/rsyslog.d/heka.conf.disabled /etc/rsyslog.d/heka.conf  
+   DEFAULT=false
+fi
+if [ "X${FORWARD_TO_FILE}" == "Xtrue" ];then
+   if [ ! -f /etc/rsyslog.d/file.conf ];then
+       ln -sf /etc/rsyslog.d/file.conf.disabled /etc/rsyslog.d/file.conf  
+   fi
+   DEFAULT=false
+fi
+if [ ${DEFAULT} == "true" ];then
+   if [ ! -f /etc/rsyslog.d/file.conf ];then
+       ln -sf /etc/rsyslog.d/file.conf.disabled /etc/rsyslog.d/file.conf  
+   fi
+fi
